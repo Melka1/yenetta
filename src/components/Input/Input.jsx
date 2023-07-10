3d804afc8adce8757c4c255c191e8a961e805b41
Melka Tolessa <63374554+Melka1@users.noreply.github.com>
2023-07-10 20:29:37 +0300

    some changes

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,10 +4,16 @@ import './styles.css'
 
 function Input({type, placeholder, hide}) {
   const [show, setShow] = useState(false)
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+
+  },[show, text])
+
   
   return (
     <div className='input--container'>
-        <input type='text' className='input' placeholder={placeholder}/>
+        <input type='text' onChange={({target})=>setText(target.value)} className='input' placeholder={placeholder}/>
         {hide&&<AiOutlineEyeInvisible className='eye' color='gray' fontSize={'1.5rem'}/>}
     </div>
   )
